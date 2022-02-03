9d383ed954617a04423387cb9e8bc93d0160e77d
OpenDoor <78300243+VishveshC@users.noreply.github.com>
2022-02-03 12:17:32 +0530

    Edited script to run on any Windows PC (After tweaking)

--- test13.bat
+++ test13.bat
@@ -1,17 +1,27 @@
 @echo off
 echo [93mPDF files to ZIP to Google Drive and Share by - Vishvesh[0m
+mkdir %TEMP%\buffer-pdf
 sleep 3
-set /p filename="File-name: "
-echo [92mSet file name is: %filename%.zip[0m
+set /P "text=Enter the file name: " 
+echo [92mSet file name is: [95m%text%.zip[0m[0m
 sleep 3
-"C:\Program Files\7-Zip\7z.exe" a "C:\Users\Black Box\Desktop\MOM\Math-papers-checked.zip" "C:\Users\Black Box\Desktop\MOM\papers"
+
+echo/
+"7Zip\7z.exe" a "%TEMP%\buffer-pdf\%text%.zip" "config\" -bso0 -bsp0
+
 echo [92mZip file made.[0m
+
 sleep 3
-"C:\Program Files\rclone-v1.57.0-windows-amd64\rclone.exe" move --verbose --progress --stats-one-line-date "C:\Users\Black Box\Desktop\MOM\Math-papers-checked.zip" GoogleD:"/test"
+
+"rclone\rclone.exe" move --progress-terminal-title --progress --stats-one-line "%TEMP%\buffer-pdf\%text%.zip" GoogleD:"/test"
 echo [92mFile UPLOAD successful, Please check G-Drive[0m
 pause
+
 sleep 3
 rmdir /s /q "C:\Users\Black Box\Desktop\MOM\papers"
 sleep 1
-mkdir "C:\Users\Black Box\Desktop\MOM\papers\"
-echo [91mLocal papers deleted, ZIP FILES SHOULD BE IN Google Drive[0m
+mkdir "C:\Users\Black Box\Desktop\MOM\papers"
+sleep 1
+rmdir /s /q %TEMP%\buffer-pdf
+echo [91mLocal papers and buffer deleted, ZIP FILES SHOULD BE IN Google Drive[0m
+pause
