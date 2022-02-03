933d5d560708f95d4b0b275e3459e6c9ab2ed86e
OpenDoor <78300243+VishveshC@users.noreply.github.com>
2022-02-03 14:52:47 +0530

    Fixes...

--- test13.bat
+++ test13.bat
@@ -1,19 +1,19 @@
 @echo off
 echo [93mPDF files to ZIP to Google Drive and Share by - Vishvesh[0m
-mkdir %TEMP%\buffer-pdf
+mkdir %TEMP%\buffer-zip
 sleep 3
 set /P "text=Enter the file name: " 
 echo [92mSet file name is: [95m%text%.zip[0m[0m
 sleep 3
 
 echo/
-"7Zip\7z.exe" a "%TEMP%\buffer-pdf\%text%.zip" "config\" -bso0 -bsp0
+"7Zip\7z.exe" a "%TEMP%\buffer-zip\%text%.zip" "C:\Users\Black Box\Desktop\MOM\papers" -bso0 -bsp0
 
 echo [92mZip file made.[0m
 
 sleep 3
 
-"rclone\rclone.exe" move --progress-terminal-title --progress --stats-one-line "%TEMP%\buffer-pdf\%text%.zip" GoogleD:"/test"
+"rclone\rclone.exe" move --progress-terminal-title --progress --stats-one-line "%TEMP%\buffer-zip\%text%.zip" GoogleD:"/test"
 echo [92mFile UPLOAD successful, Please check G-Drive[0m
 pause
 
@@ -22,6 +22,6 @@ rmdir /s /q "C:\Users\Black Box\Desktop\MOM\papers"
 sleep 1
 mkdir "C:\Users\Black Box\Desktop\MOM\papers"
 sleep 1
-rmdir /s /q %TEMP%\buffer-pdf
+rmdir /s /q %TEMP%\buffer-zip
 echo [91mLocal papers and buffer deleted, ZIP FILES SHOULD BE IN Google Drive[0m
 pause
