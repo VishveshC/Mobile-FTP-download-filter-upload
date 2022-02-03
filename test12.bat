289adbb1dd161fe7380152553936d357156a5417
OpenDoor <78300243+VishveshC@users.noreply.github.com>
2022-02-03 20:30:11 +0530

    Added %USERPROFILE% and variable desktop foldername, fixed Admin CMD CD to main folder

--- test12.bat
+++ test12.bat
@@ -1,18 +1,28 @@
 @echo off
 echo [93mMobile file transfer via FTP to local buffer by - Vishvesh[0m
-timeout 3
-mkdir %TEMP%\buffer-pdf
-echo [92mBuffer Created[0m
-timeout 3
-start cmd.exe /c "lftp -f "testfiled.lftp""
-echo [92mFile transfer successful.[0m
+cd "C:\Users\Black Box\Desktop\Dependencies"
+
 timeout 3
 
+mkdir %TEMP%\buffer-pdf
+echo [92mBuffer Created[0m
+
+timeout 3
+
+start cmd.exe /c "lftp -f "testfiled.lftp""
+echo [92mFile transfer successful.[0m
+
+timeout 3
+
+set DESKFOLDERNAME='MOM'
 set source="%TEMP%\buffer-pdf\"
-set destination="C:\Users\Black Box\Desktop\MOM\papers"
+set destination="C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers"
 for %%F in (%destination%) do set destination="%%~fF"
 for /r %source% %%F in (.) do if "%%~fF" neq %destination% ROBOCOPY "%%F" %destination% *.pdf /COPYALL /R:0 /MT[:4] /NFL /NDL /NJH /NJS
+
 timeout 1
+
 rmdir /s /q %TEMP%\buffer-pdf
-echo [91mBuffer deleted, PDF FILES SHOULD BE IN Mom/papers[0m
+echo [91mBuffer deleted, PDF FILES SHOULD BE IN MOM/papers[0m
+
 pause
--- test13.bat
+++ test13.bat
@@ -1,27 +1,35 @@
 @echo off
 echo [93mPDF files to ZIP to Google Drive and Share by - Vishvesh[0m
+
+cd "C:\Users\%USERPROFILE%\Desktop\Dependencies"
+
 mkdir %TEMP%\buffer-zip
+
 sleep 3
+set DESKFOLDERNAME='MOM'
 set /P "text=Enter the file name: " 
 echo [92mSet file name is: [95m%text%.zip[0m[0m
+
 sleep 3
 
 echo/
-"7Zip\7z.exe" a "%TEMP%\buffer-zip\%text%.zip" "C:\Users\Black Box\Desktop\MOM\papers" -bso0 -bsp0
-
-echo [92mZip file made.[0m
+"7zip\7z.exe" a "%TEMP%\buffer-zip\%text%.zip" "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers" -bso0 -bsp0
+ROBOCOPY "%TEMP%\buffer-zip" "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers-archive" *.zip /COPYALL /R:0 /MT[:4] /NFL /NDL /NJH /NJS
+echo [92mZip file made. And backup made to MOM\paper-archive[0m
 
 sleep 3
 
-"rclone\rclone.exe" move --progress-terminal-title --progress --stats-one-line "%TEMP%\buffer-zip\%text%.zip" GoogleD:"/test"
+"rclone\rclone.exe" move --progress --stats-one-line "%TEMP%\buffer-zip\%text%.zip" GoogleD:"/test"
 echo [92mFile UPLOAD successful, Please check G-Drive[0m
+
 pause
 
 sleep 3
-rmdir /s /q "C:\Users\Black Box\Desktop\MOM\papers"
+del /s /q "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers\" *.pdf
+
 sleep 1
-mkdir "C:\Users\Black Box\Desktop\MOM\papers"
-sleep 1
+
 rmdir /s /q %TEMP%\buffer-zip
 echo [91mLocal papers and buffer deleted, ZIP FILES SHOULD BE IN Google Drive[0m
+
 pause
