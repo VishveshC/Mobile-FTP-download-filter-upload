23000b09800834b2813f3ab022e684b38741466c
OpenDoor <78300243+VishveshC@users.noreply.github.com>
2022-02-04 14:29:49 +0530

    Upgraded to WinSCP. Completely standalone now.

--- test12.bat
+++ test12.bat
@@ -1,6 +1,6 @@
 @echo off
 echo [93mMobile file transfer via FTP to local buffer by - Vishvesh[0m
-cd "C:\Users\Black Box\Desktop\Dependencies"
+cd "%USERPROFILE%\Desktop\Dependencies"
 
 timeout 3
 
@@ -8,15 +8,14 @@ mkdir %TEMP%\buffer-pdf
 echo [92mBuffer Created[0m
 
 timeout 3
-
-start cmd.exe /c "lftp -f "testfiled.lftp""
+start cmd.exe /c "winscp\winscp.com /ini=nul /script="%USERPROFILE%\Desktop\Dependencies\winscptest.txt""
 echo [92mFile transfer successful.[0m
 
 timeout 3
 
-set DESKFOLDERNAME='MOM'
+set DESKFOLDERNAME=MOM
 set source="%TEMP%\buffer-pdf\"
-set destination="C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers"
+set destination="%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers"
 for %%F in (%destination%) do set destination="%%~fF"
 for /r %source% %%F in (.) do if "%%~fF" neq %destination% ROBOCOPY "%%F" %destination% *.pdf /COPYALL /R:0 /MT[:4] /NFL /NDL /NJH /NJS
 
--- test13.bat
+++ test13.bat
@@ -1,20 +1,19 @@
 @echo off
 echo [93mPDF files to ZIP to Google Drive and Share by - Vishvesh[0m
-
 cd "C:\Users\%USERPROFILE%\Desktop\Dependencies"
 
 mkdir %TEMP%\buffer-zip
 
 sleep 3
-set DESKFOLDERNAME='MOM'
+set DESKFOLDERNAME=MOM
 set /P "text=Enter the file name: " 
 echo [92mSet file name is: [95m%text%.zip[0m[0m
 
 sleep 3
 
 echo/
-"7zip\7z.exe" a "%TEMP%\buffer-zip\%text%.zip" "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers" -bso0 -bsp0
-ROBOCOPY "%TEMP%\buffer-zip" "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers-archive" *.zip /COPYALL /R:0 /MT[:4] /NFL /NDL /NJH /NJS
+"7zip\7z.exe" a "%TEMP%\buffer-zip\%text%.zip" "%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers" -bso0 -bsp0
+ROBOCOPY "%TEMP%\buffer-zip" "%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers-archive" *.zip /COPYALL /R:0 /MT[:4] /NFL /NDL /NJH /NJS
 echo [92mZip file made. And backup made to MOM\paper-archive[0m
 
 sleep 3
@@ -25,7 +24,7 @@ echo [92mFile UPLOAD successful, Please check G-Drive[0m
 pause
 
 sleep 3
-del /s /q "C:\Users\%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers\" *.pdf
+del /s /q "%USERPROFILE%\Desktop\%DESKFOLDERNAME%\papers\" *.pdf
 
 sleep 1
 
